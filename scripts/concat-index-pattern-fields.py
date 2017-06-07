21258bccefa6bf0c0aa0584a3f8a53055bd52ad4
Rich Megginson <rmeggins@redhat.com>
2017-06-06 20:03:48 -0600

    remove duplicate names from index pattern fields

--- scripts/concat-index-pattern-fields.py
+++ scripts/concat-index-pattern-fields.py
@@ -4,14 +4,26 @@ import sys
 import json
 
 doc = {}
+names = {}
+
+def removedupnames(fieldary):
+    ret = []
+    for field in fieldary:
+        if not field['name'] in names:
+            names[field['name']] = True
+            ret.append(field)
+    return ret
+
 for filename in sys.argv[1:]:
     with open(filename, "r") as ff:
         if doc:
             d2 = json.load(ff)
-            d2fieldary = json.loads(d2['fields'])
+            d2fieldary = removedupnames(json.loads(d2['fields']))
             docfieldary = json.loads(doc['fields'])
             docfieldary.extend(d2fieldary)
             doc['fields'] = json.dumps(docfieldary)
         else:
             doc = json.load(ff)
-sys.stdout.write(json.dumps(doc, indent=4, sort_keys=True))
+            docfieldary = removedupnames(json.loads(doc['fields']))
+            doc['fields'] = json.dumps(docfieldary)
+sys.stdout.write(json.dumps(doc, indent=4, sort_keys=True, separators=(',', ': ')))
